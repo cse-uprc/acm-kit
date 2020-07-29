b9ab93d260623e2d537f9361b00c176d59f2d759
Sam Butler <67570772+sambutler1017@users.noreply.github.com>
2020-07-28 21:54:16 -0400

    ACMKIT: Incremental Release -- version bump (#9)

---
+++ dist/acmkit-lib/lib/service/math.service.d.ts
@@ -0,0 +1,7 @@
+export declare class MathService {
+    constructor();
+    addNumbers(num1: any, num2: any): any;
+    subtractNumbers(num1: any, num2: any): number;
+    multiplyNumbers(num1: any, num2: any): number;
+    addThreeNumbers(num1: any, num2: any, num3: any): any;
+}
