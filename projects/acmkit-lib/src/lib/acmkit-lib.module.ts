db9cd4b30884dcb2e5c47b678a8eb7ac401b4164
Samuel Butler <sambutler1017@icloud.com>
2020-07-30 15:24:26 -0400

    Updating card component

--- projects/acmkit-lib/src/lib/acmkit-lib.module.ts
+++ projects/acmkit-lib/src/lib/acmkit-lib.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AcmkitLibComponent } from './acmkit-lib.component';
-import { AngularTestTitleComponent } from './components/angular-test-title/angular-test-title.component';
 import { CardComponent } from './components/card/card.component';
-import { MathService } from './service/math.service';
 
 @NgModule({
-  declarations: [AcmkitLibComponent, AngularTestTitleComponent, CardComponent],
+  declarations: [AcmkitLibComponent, CardComponent],
   imports: [BrowserModule, RouterModule],
-  exports: [AcmkitLibComponent, AngularTestTitleComponent, CardComponent],
-  providers: [MathService],
+  exports: [AcmkitLibComponent, CardComponent],
+  providers: [],
 })
 export class AcmkitLibModule {}
