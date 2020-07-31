4709128caa11c1e6b58075624dc0739e44eaa112
Samuel Butler <sambutler1017@icloud.com>
2020-07-30 23:50:33 -0400

    Adding base page and cleaning up code

--- projects/acmkit-lib/src/public-api.ts
+++ projects/acmkit-lib/src/public-api.ts
@@ -4,6 +4,7 @@
 export * from './lib/acmkit-lib.component';
 export * from './lib/acmkit-lib.module';
 export * from './lib/acmkit-lib.service';
+export * from './lib/components/base-page/base-page.component';
 /**
  * Components
  */
