fdc02fec046120b45eee279be4b13c5c9750b29a
Sam Butler <67570772+sambutler1017@users.noreply.github.com>
2020-08-01 10:52:31 -0400

    Adding base page and cleaning up code (#28)

--- projects/acmkit-lib/src/lib/acmkit-lib.module.ts
+++ projects/acmkit-lib/src/lib/acmkit-lib.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AcmkitLibComponent } from './acmkit-lib.component';
+import { BasePageComponent } from './components/base-page/base-page.component';
 import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [AcmkitLibComponent, CardComponent],
+  declarations: [AcmkitLibComponent, CardComponent, BasePageComponent],
   imports: [BrowserModule, RouterModule],
-  exports: [AcmkitLibComponent, CardComponent],
+  exports: [AcmkitLibComponent, CardComponent, BasePageComponent],
   providers: [],
 })
 export class AcmkitLibModule {}
