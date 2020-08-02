eff0ee597fa08cec6569f86b0af3a659aaa2bef0
Luke Lengel <Steven.Lengel@rockets.utoledo.edu>
2020-08-01 21:17:57 -0400

    Delete login-card.component.d.ts

--- dist/acmkit-lib/lib/components/login-card/login-card.component.d.ts
+++
@@ -1,8 +0,0 @@
-import { OnInit } from '@angular/core';
-import * as i0 from "@angular/core";
-export declare class LoginCardComponent implements OnInit {
-    constructor();
-    ngOnInit(): void;
-    static ɵfac: i0.ɵɵFactoryDef<LoginCardComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDefWithMeta<LoginCardComponent, "ak-login-card", never, {}, {}, never, never>;
-}
