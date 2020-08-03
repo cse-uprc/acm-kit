cad3252aaa685fe709be675ed407e57a66072b3f
Samuel Butler <sambutler1017@icloud.com>
2020-08-03 15:07:49 -0400

    Adding base landing page

--- projects/acmkit-lib/src/lib/acmkit-lib.module.ts
+++ projects/acmkit-lib/src/lib/acmkit-lib.module.ts
@@ -4,12 +4,25 @@ import { RouterModule } from '@angular/router';
 import { AcmkitLibComponent } from './acmkit-lib.component';
 import { BasePageComponent } from './components/base-page/base-page.component';
 import { CardComponent } from './components/card/card.component';
+import { LandingComponent } from './components/landing/landing.component';
 import { LoginCardComponent } from './components/login-card/login-card.component';
 
 @NgModule({
-  declarations: [AcmkitLibComponent, CardComponent, BasePageComponent, LoginCardComponent],
+  declarations: [
+    AcmkitLibComponent,
+    CardComponent,
+    BasePageComponent,
+    LoginCardComponent,
+    LandingComponent,
+  ],
   imports: [BrowserModule, RouterModule],
-  exports: [AcmkitLibComponent, CardComponent, BasePageComponent, LoginCardComponent],
+  exports: [
+    AcmkitLibComponent,
+    CardComponent,
+    BasePageComponent,
+    LoginCardComponent,
+    LandingComponent,
+  ],
   providers: [],
 })
 export class AcmkitLibModule {}
--- projects/acmkit-lib/src/lib/components/login-card/login-card.component.spec.ts
+++ projects/acmkit-lib/src/lib/components/login-card/login-card.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { CardComponent } from 'acmkit-lib';
 import { LoginCardComponent } from './login-card.component';
 
 describe('LoginCardComponent', () => {
@@ -8,9 +9,9 @@ describe('LoginCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginCardComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [LoginCardComponent, CardComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
