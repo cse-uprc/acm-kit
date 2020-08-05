56561696c09ff8674bcee360863bfb5ef7721d8c
Sam Butler <67570772+sambutler1017@users.noreply.github.com>
2020-08-05 15:53:53 -0400

    Cleaning up code and adding css to account for small screens (#42)

--- projects/acmkit-lib/src/lib/components/landing/landing.component.ts
+++ projects/acmkit-lib/src/lib/components/landing/landing.component.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ak-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
