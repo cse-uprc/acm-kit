b5b8f2ac43e083593f5eb1cd83dd51bdf3438abd
sambutler1017 <sambutler1017@icloud.com>
2020-08-08 21:29:17 -0400

    ACMKIT: Incremental Release -- version bump

--- dist/acmkit-lib/lib/services/auth/auth.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-export declare class AuthService {
-    private http;
-    constructor(http: HttpClient);
-    authenticate(username: string, password: string): void;
-}
