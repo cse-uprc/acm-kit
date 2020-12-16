0ec48ec912b883832976621c0a55de46f43088ad
DESKTOP-0MRSA9E\sambu <sambutler1017@icloud.com>
2020-12-16 17:24:58 -0500

    Environemnt Hotfix

--- projects/acmkit-lib/src/lib/assets/globals.ts
+++ projects/acmkit-lib/src/lib/assets/globals.ts
@@ -4,8 +4,8 @@ export class Environment {
   public static LOCAL_HOST = 'localhost:8080';
   public static HEROKU_URI = 'herokuapp.com';
 
-  public static PRODUCTION_PATH = 'acm-ui-dev';
-  public static DEVELOPMENT_PATH = 'acm-web';
+  public static PRODUCTION_PATH = 'acm-web';
+  public static DEVELOPMENT_PATH = 'acm-ui-dev';
   public static LOCAL_PATH = 'localhost:4200';
 
   public static SOCKET_ENDPOINT = '/api/web-notification-app/websocket';
