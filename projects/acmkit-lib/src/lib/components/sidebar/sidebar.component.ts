3241f6c366594e2f64655a1b5f6009e13a767414
DESKTOP-0MRSA9E\sambu <sambutler1017@icloud.com>
2021-01-04 17:44:37 -0500

    Adding height

--- projects/acmkit-lib/src/lib/components/sidebar/sidebar.component.ts
+++ projects/acmkit-lib/src/lib/components/sidebar/sidebar.component.ts
@@ -6,19 +6,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SiderbarComponent {
-  @Input() items = [
-    { name: 'Dashboard', icon: 'house' },
-    { name: 'Classroom', icon: 'house' },
-    { name: 'Resources', icon: 'house' },
-    { name: 'Teachers', icon: 'house' },
-    { name: 'Student Advisors', icon: 'house' },
-    { name: 'Video Lectures', icon: 'house' },
-    { name: 'Calendar Events', icon: 'house' },
-    { name: 'Event Sign Up', icon: 'house' },
-    { name: 'ACM Information', icon: 'house' },
-    { name: 'Bot Service Info', icon: 'house' },
-    { name: 'About Us', icon: 'house' },
-    { name: 'Contact Services', icon: 'house' },
-  ];
+  @Input() items;
   constructor() {}
 }
