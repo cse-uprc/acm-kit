800a3c39337949d0461a257ac63f505b3e1b07ca
DESKTOP-0MRSA9E\sambu <sambutler1017@icloud.com>
2021-01-04 17:19:00 -0500

    ACMKIT: Incremental Release -- version bump

--- dist/acmkit-lib/acmkit-lib.d.ts
+++ dist/acmkit-lib/acmkit-lib.d.ts
@@ -2,5 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
+export { HouseIconComponent as ɵc } from './lib/assets/icons/house/house.component';
+export { IconComponent as ɵd } from './lib/assets/icons/icon.component';
 export { UrlService as ɵa } from './lib/services/url/url.service';
 export { UserService as ɵb } from './lib/services/user/user.service';
